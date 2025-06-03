451214f18dca7c11e5373527e58a01c45d9a62d8
Luis Enrique Plata Osorio <www.luisplata@gmail.com>
2025-06-03 21:54:27 +0000

    puedes migrar esto a pages? mi despliegue seran archivos estaticos

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', // Habilita la exportación estática
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true, // Necesario para 'output: export' si no se usa un loader personalizado
     remotePatterns: [
       {
         protocol: 'https',
