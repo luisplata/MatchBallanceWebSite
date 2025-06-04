d56f19d842596cd6f46b0214aca860814ea8ea72
Luis Enrique Plata Osorio <www.luisplata@gmail.com>
2025-06-04 04:13:23 +0000

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,23 +6,11 @@ import Footer from '@/components/layout/footer';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
-// Definición de textos estáticos para el Header (actualizada)
-const navDictionary = {
-  home: "Inicio",
-  news: "Noticias",
-  forum: "Foro",
-  // "press" ha sido eliminado
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  // Manejar el locale para _app.tsx, necesario para el Header si no se pasa desde pageProps.
-  // En un sitio solo en español, esto es menos crítico pero bueno tenerlo si se reintroduce i18n.
-  const currentLocale = 'es'; // Forzado a español
 
   return (
     <>
-      <Header navDictionary={navDictionary} />
+      <Header />
       <main className="flex-grow">
         <Component {...pageProps} />
       </main>
