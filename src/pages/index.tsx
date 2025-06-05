e407b8949e9d7683d744fcb91dbdba81e016d3ed
Luis Enrique Plata Osorio <www.luisplata@gmail.com>
2025-06-05 19:28:59 +0000

    Ya puedes quitarle las lineas de los contenedores :)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,12 +19,12 @@ export default function HomePage() {
         <div className="relative flex-grow flex flex-col p-4 md:p-8">
 
           {/* Main Content Area: Arranges Left Stack (Label + Logo) and Right (Mobile) */}
-          <div className="flex-grow flex flex-col md:flex-row items-center md:items-stretch justify-center md:justify-between w-full max-w-screen-xl mx-auto pt-8 md:pt-12 pb-20 md:pb-24 md:px-12">
+          <div className="flex-grow flex flex-col md:flex-row items-center md:items-stretch justify-center md:justify-between w-full max-w-screen-xl mx-auto pt-8 md:pt-12 pb-20 md:pb-24">
 
             {/* Left Stack: Label over Logo */}
-            <div className="flex flex-col items-center md:items-start space-y-4 md:space-y-8 md:w-3/5 border border-dashed border-red-500">
+            <div className="flex flex-col items-center md:items-start space-y-4 md:space-y-8 md:w-3/5">
               {/* Label Container */}
-              <div className="transform transition-transform hover:scale-105 w-full md:-ml-[50%] border border-dashed border-blue-500">
+              <div className="transform transition-transform hover:scale-105 w-full md:-ml-[50%]">
                 <Image
                   src="/images/Label.png"
                   alt="MatchBallance Studios"
@@ -36,7 +36,7 @@ export default function HomePage() {
                 />
               </div>
               {/* Logo Container */}
-              <div className="transform transition-transform hover:scale-105 w-full border border-dashed border-blue-500 md:flex md:flex-grow md:items-center md:justify-center">
+              <div className="transform transition-transform hover:scale-105 w-full md:flex md:flex-grow md:items-center md:justify-center">
                 <Image
                   src="/images/LogoHero.png"
                   alt="Match Ballance The Game"
@@ -50,7 +50,7 @@ export default function HomePage() {
             </div>
 
             {/* Right Column: Phone Mockup */}
-            <div className="md:w-2/5 flex justify-center border border-dashed border-red-500">
+            <div className="md:w-2/5 flex justify-center">
               <Image
                 src="/images/Movile.jpg"
                 alt="Game on Phone Screen"
